cae409aaa040189542790d398a3363c6b9b46327
paty-oliveira <dev.patricia.oliveira@gmail.com>
2024-11-26 18:17:46 +0000

    Fix linting rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+
 import { Button } from "@chakra-ui/react";
+import { useState } from "react";
+
+import viteLogo from "/vite.svg";
+
+import reactLogo from "./assets/react.svg";
 
 function App() {
   const [count, setCount] = useState(0);
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import { ThemeProvider } from "next-themes";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
