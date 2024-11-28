c308680654e209b40575ee3857c65508b6ca08e8
paty-oliveira <dev.patricia.oliveira@gmail.com>
2024-11-28 14:51:17 +0000

    Add vike config for pre-rendering

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+// import vike from "vike/plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    // vike({
+    //   prerender: true,
+    // }),
+  ],
 });
