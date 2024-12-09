8b588718c835b4e447bc929a52cc1d1fdc0d24f1
paty-oliveira <dev.patricia.oliveira@gmail.com>
2024-12-09 17:39:24 +0000

    Update styling in work page

--- src/pages/projectId/index.tsx
+++ src/pages/projectId/index.tsx
@@ -1,8 +1,33 @@
-const Project = () => {
+import "../../styles/shared.css";
+
+import { FC } from "react";
+import { Helmet } from "react-helmet";
+import { useParams } from "react-router-dom";
+
+import Header from "@/components/header";
+import { cms } from "@/content";
+import { seo } from "@/content/seo";
+
+const Project: FC = () => {
+  const { projectId } = useParams();
+  const { header, projectsPage } = cms;
+  const { projects: metadata } = seo;
+
+  const project = projectsPage.projects.find(
+    (project) => project.id === projectId
+  );
+
   return (
-    <>
-      <h1>Project Name page</h1>
-    </>
+    <main className="main-layout">
+      <Helmet>
+        <title>{metadata.title}</title>
+        <meta name={metadata.name} content={project?.description} />
+      </Helmet>
+      <Header content={header.content} />
+      <section>
+        <p>Content about the project: {projectId}</p>
+      </section>
+    </main>
   );
 };
 
